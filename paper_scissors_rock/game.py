cc7862808d8d87be6bdaffef4a81aac9b08eb0f4
sstollenwerk <sstollenwerk@yahoo.co.nz>
2021-04-25 13:37:32 +1200

    make game internals

--- paper_scissors_rock/game.py
+++ paper_scissors_rock/game.py
@@ -1,12 +1,21 @@
 from enum import Enum
-from typing import Optional
+from typing import Optional, Callable
+import random
 
 class Move(Enum):
     PAPER = 0
     SCISSORS = 1
     ROCK = 2
 
+class  Player:
+    def __init__ (self, turn:Turn):
+        self.turn = turn
+
+
+
+Turn = Callable[[], Move]
 Possibility = Optional[bool]
+##Display:Callable[[Move, Move], None]
 
 def _round(p1:Move, p2:Move) -> Possibility:
     if p1 == p2: return None
@@ -17,3 +26,14 @@ def rev(p:Possibility) -> Possibility:
         return None
     else:
         return not(p)
+
+
+def rand():
+    return  random.choice(list(Move))
+
+BASIC_AI = Player(rand)
+
+def round(p1:Player, p2:Player, move_display:Callable[[Move, Move], None]) -> Possibility:
+    moves = [p1.turn(), p2.turn()]
+    move_display(*moves)
+    return _round(*moves)
